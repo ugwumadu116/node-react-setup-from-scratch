ba4778bd58795d9b2fa1597ea1b23d709506e0a4
ugwumadu116 <ugwumadu116@gmail.com>
2019-08-10 19:19:20 +0100

    setup enzyme

---
+++ try/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React setup with webpack ready</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
